d2a4c5f68605c04b6e02ee225b7d26d552af340a
Wei Shen <shenwei356@gmail.com>
2020-12-06 17:47:03 +0800

    search: using binary search for finding some kmers in target genomes

--- kmcp/cmd/util-db-info.go
+++ kmcp/cmd/util-db-info.go
@@ -67,7 +67,7 @@ type UnikIndexDBInfo struct {
 	NumNames   int      `yaml:"numNames"`
 	Names      []string `yaml:"names"`
 	Sizes      []uint64 `yaml:"kmers"`
-	Paths      []string `yaml:"unik-path"`
+	Paths      []string `yaml:"unik-paths"`
 
 	path         string
 	NameMapping  map[string]string
